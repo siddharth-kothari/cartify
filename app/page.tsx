3c3a6cb927a59d035a8a191def2485793011b5cd
Siddharth Kothari <sidkothari005@gmail.com>
2023-11-04 18:03:17 +0530

    inital commit

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Banner from "@/Components/Banner";
+import ProductFeed from "@/Components/ProductFeed";
+import { api } from "./api";
+import axios from "axios";
+
+export const getAllProducts = async () => {
+  const res = await api.get("/products");
+  return res;
+};
+
+export default async function Home() {
+  const { data } = await getAllProducts();
+
+  const products = data.products;
+
+  return (
+    <main>
+      {/* Banner */}
+      <Banner />
+      {/* Produts */}
+      <ProductFeed products={products} />
+    </main>
+  );
+}
