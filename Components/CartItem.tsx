b9d6720ec7a04255e41034aa54b5184bd04f87ac
Siddharth Kothari <sidkothari005@gmail.com>
2023-11-30 19:56:57 +0530

    cart page UI

--- Components/CartItem.tsx
+++ Components/CartItem.tsx
@@ -19,50 +19,53 @@ const CartItem = ({ item }: any) => {
   };
 
   return (
-    <div className="grid grid-cols-5 border-b pb-5">
-      <Image
-        src={item.image}
-        alt={item.name}
-        width={200}
-        height={200}
-        className=" object-contain"
-      />
-
-      <div className="col-span-3 mx-5 grid gap-y-5 place-content-start">
-        <Link href={`/product/${item.id}`} className="text-black">
-          {item.name}
-        </Link>
-        <div className="flex space-x-5 items-center">
-          <div className="flex space-x-2 items-center">
-            <button
-              className="button w-8 h-8 p-1"
-              onClick={handleDecreaseQuantity}
-            >
-              <MinusIcon />
-            </button>
-            <p className="text-black">{item.qty}</p>
-            <button
-              className="button w-8 h-8 p-1 "
-              onClick={handleIncreaseQuantity}
-            >
-              <PlusIcon />
-            </button>
-          </div>
-
-          <button
-            onClick={() => removeItemToCart()}
-            className="mt-auto button w-max h-max"
-          >
-            Remove
-          </button>
-        </div>
+    <div className="flex border-b pb-5">
+      <div className="w-[30%]">
+        <Image
+          src={item.image}
+          alt={item.name}
+          width={200}
+          height={200}
+          className="object-contain"
+        />
       </div>
 
-      <div className="flex justify-center">
+      <div className="mx-5 sm:flex justify-between space-y-5 gap-y-5 place-content-start w-[70%]">
+        <div className="space-y-5">
+          <Link href={`/product/${item.id}`} className="text-black">
+            {item.name}
+          </Link>
+          <div className="flex space-x-5 items-center">
+            <div className="flex space-x-2 items-center">
+              <button
+                className="button w-8 h-8 p-1"
+                onClick={handleDecreaseQuantity}
+              >
+                <MinusIcon />
+              </button>
+              <p className="text-black">{item.qty}</p>
+              <button
+                className="button w-8 h-8 p-1 "
+                onClick={handleIncreaseQuantity}
+              >
+                <PlusIcon />
+              </button>
+            </div>
+
+            <button
+              onClick={() => removeItemToCart()}
+              className="mt-auto button w-max h-max"
+            >
+              Remove
+            </button>
+          </div>
+        </div>
         <p className="text-black">
           ₹ <span>{item.price * 80}</span>
         </p>
       </div>
+
+      <div className="flex justify-center"></div>
     </div>
   );
 };
