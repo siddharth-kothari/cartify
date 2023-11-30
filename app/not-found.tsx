6510986814c36122986af76ea3b4c56adda020ae
Siddharth Kothari <sidkothari005@gmail.com>
2023-11-30 10:57:48 +0530

    404 page added

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center h-[75vh] gap-y-5 px-5">
+      <h2 className="font-abril text-3xl md:text-4xl">404</h2>
+      <p className="font-bold text-3xl md:text-5xl text-center">
+        Uh oh. his page doesn't exists.
+      </p>
+      <Link href="/" className="button">
+        Back to home
+      </Link>
     </div>
   );
 }
