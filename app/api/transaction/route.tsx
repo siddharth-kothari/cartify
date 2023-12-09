b6f9fdf809ec085ef5dabe778e91b7b7f29078d3
Siddharth Kothari <sidkothari005@gmail.com>
2023-12-09 19:09:36 +0530

    test v3

--- app/api/transaction/route.tsx
+++ app/api/transaction/route.tsx
@@ -9,6 +9,30 @@ export async function POST(req: Request) {
       const { data } = await req.json();
       console.log("Received data:", data.order.order_id);
 
+      const inputDateString = data.payment.payment_time;
+      const inputDate = new Date(inputDateString);
+
+      const year = inputDate.getFullYear();
+      const month = String(inputDate.getMonth() + 1).padStart(2, "0");
+      const day = String(inputDate.getDate()).padStart(2, "0");
+      const hours = String(inputDate.getHours()).padStart(2, "0");
+      const minutes = String(inputDate.getMinutes()).padStart(2, "0");
+      const seconds = String(inputDate.getSeconds()).padStart(2, "0");
+
+      const outputDateString = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      console.log(outputDateString);
+
+      const [updateOrder]: any = await pool.execute(
+        "UPDATE orders SET status = ?, method = ?, bank_ref = ?, updated_at = ? WHERE orderno = ?",
+        [
+          data.payment.payment_status,
+          data.payment.payment_group,
+          data.payment.bank_reference,
+          outputDateString,
+          data.order.order_id,
+        ]
+      );
+
       // Perform any processing or database operations with the data
 
       // Send a response
