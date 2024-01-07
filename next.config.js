d6e13fce886739d7f0102fb7ceeffabb7cd9161c
Siddharth Kothari <sidkothari005@gmail.com>
2024-01-07 15:42:52 +0530

    fix: product image bug resolved

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["https://links.papareact.com/", "fakestoreapi.com", "i.dummyjson.com"]
+        domains: ["https://links.papareact.com/", "fakestoreapi.com", "i.dummyjson.com", "cdn.dummyjson.com"]
     }
 }
 
