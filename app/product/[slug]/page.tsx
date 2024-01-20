e9165991c76561614bb16c38be421fffc4969747
Siddharth Kothari <sidkothari005@gmail.com>
2024-01-20 22:26:27 +0530

    Product SEO

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,6 +1,24 @@
 import ProductDetail from "@/Components/ProductDetail";
 import { api } from "@/app/api";
 
+export async function generateMetadata({ params }: any) {
+  const { slug } = params;
+  const { data } = await api.get(
+    `${process.env.NEXT_PUBLIC_STORE_BASE_URL}products/${slug}`
+  );
+
+  return {
+    title: data.title + " | " + data.brand + " | " + data.category,
+    description: data.description,
+    openGraph: {
+      title: data.title,
+      description: data.description,
+      type: "website",
+      images: [data.thumbnail],
+    },
+  };
+}
+
 export default async function ProductPage({ params }: any) {
   const { slug } = params;
   const { data } = await api.get(
